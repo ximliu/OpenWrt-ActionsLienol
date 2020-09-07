cadfff278fcdded86033d75728c6634ad2005649
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2020-09-07 23:20:42 +0800

    Update OpenWrt-x86-64-Lienol.sh

--- OpenWrt-x86-64-Lienol.sh
+++ OpenWrt-x86-64-Lienol.sh
@@ -23,6 +23,7 @@ git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/liwenjie119/luci-app-koolproxyR package/myapp/luci-app-koolproxyR
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge -b 18.06
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/diy/luci-theme-argon
 
 # 替换更新passwall和ssrplus+
 rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
